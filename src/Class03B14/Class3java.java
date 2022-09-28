dec5a809486e14fd5bdfd693315e431265795cb2
Masiha12 <abdullahmasiha44@gmail.com>
2022-09-27 22:10:51 -0400

    commit 1

---
+++ src/Class03B14/Class3java.java
@@ -0,0 +1,14 @@
+package Class03B14;
+
+public class Class3java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hey this is a git class");
+		System.out.println("hey this is a change in my project");
+		System.out.println("how are you");
+	}
+
+}
